ba9876e043cd2389e5a6f7f30eff3d0499886501
naiweilanlan <2632649293@qq.com>
2025-01-04 13:01:21 +0800

    HelloCpp

---
+++ Test/HelloCpp/HelloCpp.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 86138 on 2025/1/4.
+//
+
+#include <iostream>
+
+int main()
+{
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++)
+    {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
