fa22d06054c0777f66815f38c94480496ba2c111
Naiweilanlan <2632649293@qq.com>
2025-06-02 15:34:57 +0800

    CSP_602

--- Test/CSP/31_01.cpp
+++ Test/CSP/31_01.cpp
@@ -3,46 +3,53 @@
 //
 
 #include <iostream>
-#include <string>
 #include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <list>
-#include <algorithm>
-#include <cmath>
-#include <functional>
-#include <numeric>
 
 using namespace std;
 
 class Solution {
 public:
-    // 题目要求的返回值类型 和方法名
-    // 根据题目定义参数
-    // 示例: vector<int> twoSum(vector<int>& nums, int target) {
-    //     // 实现算法逻辑
-    //     return result; // 返回结果
-    // }
+  vector<pair<int, int>> reshape(int n, int m, vector<pair<int, int>>& fuc, vector<pair<int, int>>& init) {
+    // 计算一次函数累加和
+    int dx = 0, dy = 0;
+    for (int i = 0; i < n; ++i) {
+      dx += fuc[i].first;
+      dy += fuc[i].second;
+    }
+
+    vector<pair<int, int>> res(m);
+    for (int i = 0; i < m; ++i) {
+      res[i].first = init[i].first + dx;
+      res[i].second = init[i].second + dy;
+    }
+    return res;
+  }
 };
 
 int main() {
-    // IO优化
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    // 读取输入，根据题目格式
-    // 示例: int n; cin >> n; vector<int> nums(n); for(int i = 0; i < n; i++) cin >> nums[i];
+  int n, m;
+  cin >> n >> m;
 
-    Solution sol;
-    // 调用方法，获取结果
-    // 示例: auto result = sol.twoSum(nums, target);
+  vector<pair<int, int>> fuc(n);
+  vector<pair<int, int>> init(m);
 
-    // 输出结果，根据题目要求
-    // 示例: for(int x : result) cout << x << " "; cout << endl;
+  for (int i = 0; i < n; i++) {
+    cin >> fuc[i].first >> fuc[i].second;
+  }
 
-    return 0;
+  for (int i = 0; i < m; i++) {
+    cin >> init[i].first >> init[i].second;
+  }
+
+  Solution sol;
+  auto ans = sol.reshape(n, m, fuc, init);
+
+  for (auto& p : ans) {
+    cout << p.first << " " << p.second << "\n";
+  }
+
+  return 0;
 }
--- Test/CSP/31_02.cpp
+++ Test/CSP/31_02.cpp
@@ -3,46 +3,82 @@
 //
 
 #include <iostream>
-#include <string>
 #include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <list>
-#include <algorithm>
 #include <cmath>
-#include <functional>
-#include <numeric>
+#include <iomanip>
 
 using namespace std;
 
-class Solution {
-public:
-    // 题目要求的返回值类型 和方法名
-    // 根据题目定义参数
-    // 示例: vector<int> twoSum(vector<int>& nums, int target) {
-    //     // 实现算法逻辑
-    //     return result; // 返回结果
-    // }
+const double PI = acos(-1.0);
+
+// 定义仿射变换矩阵
+struct Matrix {
+    double a, b, c, d; // 对应 2x2 矩阵的四个元素
+    Matrix(double a_ = 1, double b_ = 0, double c_ = 0, double d_ = 1) : a(a_), b(b_), c(c_), d(d_) {}
+
+    // 矩阵乘法
+    Matrix operator*(const Matrix& other) const {
+        return Matrix(
+            a * other.a + b * other.c,
+            a * other.b + b * other.d,
+            c * other.a + d * other.c,
+            c * other.b + d * other.d
+        );
+    }
+
+    // 计算逆矩阵
+    Matrix inverse() const {
+        double det = a * d - b * c;
+        return Matrix(d / det, -b / det, -c / det, a / det);
+    }
+
+    // 应用变换到点 (x, y)
+    pair<double, double> apply(double x, double y) const {
+        return {a * x + b * y, c * x + d * y};
+    }
 };
 
 int main() {
-    // IO优化
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 
-    // 读取输入，根据题目格式
-    // 示例: int n; cin >> n; vector<int> nums(n); for(int i = 0; i < n; i++) cin >> nums[i];
+    int n, m;
+    cin >> n >> m;
 
-    Solution sol;
-    // 调用方法，获取结果
-    // 示例: auto result = sol.twoSum(nums, target);
+    vector<Matrix> prefix(n + 1); // prefix[0] = 单位矩阵
+    for (int i = 1; i <= n; ++i) {
+        int type;
+        double val;
+        cin >> type >> val;
+        if (type == 1) {
+            // 拉伸：k 倍放大，相当于乘以 k
+            Matrix scale(val, 0, 0, val);
+            prefix[i] = scale * prefix[i - 1];
+        } else {
+            // 旋转 θ 弧度
+            Matrix rotate(cos(val), -sin(val), sin(val), cos(val));
+            prefix[i] = rotate * prefix[i - 1];
+        }
+    }
 
-    // 输出结果，根据题目要求
-    // 示例: for(int x : result) cout << x << " "; cout << endl;
+    // 读取所有查询，统一处理
+    vector<tuple<int, int, int, int>> queries(m);
+    for (int i = 0; i < m; ++i) {
+        int l, r, x, y;
+        cin >> l >> r >> x >> y;
+        queries[i] = {l, r, x, y};
+    }
+
+    vector<pair<double, double>> result;
+    for (auto [l, r, x, y] : queries) {
+        Matrix trans = prefix[r] * prefix[l - 1].inverse();
+        result.push_back(trans.apply(x, y));
+    }
+
+    cout << fixed << setprecision(10); // 设置精度
+    for (auto [x, y] : result) {
+        cout << x << " " << y << "\n";
+    }
 
     return 0;
 }
